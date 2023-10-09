51bb2d7c877077809f769b8d6d3ccf0604883412
Diamantino <Diamantino.op@gmail.com>
2023-10-09 10:06:49 +0200

    Save/Load

--- core/src/main/java/com/diamantino/voxelcraft/common/entities/Entity.java
+++ core/src/main/java/com/diamantino/voxelcraft/common/entities/Entity.java
@@ -4,12 +4,16 @@ import com.badlogic.gdx.math.Vector3;
 import com.diamantino.voxelcraft.common.blocks.BlockPos;
 import com.diamantino.voxelcraft.common.world.World;
 
-public class Entity {
+public abstract class Entity {
     public Vector3 entityPos;
     public BlockPos blockPos;
 
     public Entity(World world, BlockPos spawnLocation) {
         this.blockPos = spawnLocation;
         this.entityPos = spawnLocation.toVec3();
+    }
+
+    public byte[] saveData() {
+        return new byte[]{};
     }
 }
