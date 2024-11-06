6136b348036b9d604fa7dea29ff485aeced3076e
Gitea <diamantino.op@gmail.com>
2024-11-06 22:55:58 +0100

    Update

--- core/src/main/java/com/diamantino/voxelcraft/client/screens/MainMenuScreen.java
+++ core/src/main/java/com/diamantino/voxelcraft/client/screens/MainMenuScreen.java
@@ -58,7 +58,6 @@ public class MainMenuScreen implements Screen {
             @Override
             public boolean touchDown(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y, int pointer, int button) {
 
-
                 return true;
             }
         });
