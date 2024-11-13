f87b1bb4cd3abb265480ee4bd0d393cef166ee82
Diamantino <Diamantino.op@gmail.com>
2024-11-13 13:21:41 +0100

    Update

--- core/src/main/java/com/diamantino/voxelcraft/client/utils/GuiHelper.java
+++ core/src/main/java/com/diamantino/voxelcraft/client/utils/GuiHelper.java
@@ -1,5 +1,6 @@
 package com.diamantino.voxelcraft.client.utils;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.Widget;
 
 /**
@@ -8,6 +9,14 @@ import com.badlogic.gdx.scenes.scene2d.ui.Widget;
  * @author Diamantino
  */
 public class GuiHelper {
+    public static void setObjectPosition(Widget widget, int x, int y) {
+        setObjectPosition(widget, x, y, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    }
+
+    public static void setObjectPosition(Widget widget, int x, int y, boolean centeredX, boolean centeredY) {
+        setObjectPosition(widget, x, y, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), centeredX, centeredY);
+    }
+
     public static void setObjectPosition(Widget widget, int x, int y, int screenWidth, int screenHeight, boolean centeredX, boolean centeredY) {
         if (centeredX) {
             x = Math.round(((float) screenWidth / 2) - (widget.getWidth() / 2)) + x;
@@ -21,10 +30,58 @@ public class GuiHelper {
     }
 
     public static void setObjectPosition(Widget widget, int x, int y, int screenWidth, int screenHeight) {
+        float scaleX = screenWidth / 1920f;
+        float scaleY = screenHeight / 1080f;
 
+        widget.setPosition(Math.round(x * scaleX), Math.round(y * scaleY));
     }
 
-    public static void setObjectSize(Widget widget, int width, int height, int screenWidth, int screenHeight, float guiScale) {
+    public static void setObjectSizeX(Widget widget, int width, int height, float guiScale, boolean perfectScale) {
+        setObjectSize(widget, width, height, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), guiScale, true, false, perfectScale);
+    }
 
+    public static void setObjectSizeX(Widget widget, int width, int height, int screenWidth, int screenHeight, float guiScale, boolean perfectScale) {
+        setObjectSize(widget, width, height, screenWidth, screenHeight, guiScale, true, false, perfectScale);
+    }
+
+    public static void setObjectSizeY(Widget widget, int width, int height, float guiScale, boolean perfectScale) {
+        setObjectSize(widget, width, height, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), guiScale, false, true, perfectScale);
+    }
+
+    public static void setObjectSizeY(Widget widget, int width, int height, int screenWidth, int screenHeight, float guiScale, boolean perfectScale) {
+        setObjectSize(widget, width, height, screenWidth, screenHeight, guiScale, false, true, perfectScale);
+    }
+
+    public static void setObjectSize(Widget widget, int width, int height, float guiScale) {
+        setObjectSize(widget, width, height, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), guiScale, false, false, false);
+    }
+
+    public static void setObjectSize(Widget widget, int width, int height, float guiScale, boolean scaleX, boolean scaleY) {
+        setObjectSize(widget, width, height, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), guiScale, scaleX, scaleY, false);
+    }
+
+    public static void setObjectSize(Widget widget, int width, int height, int screenWidth, int screenHeight, float guiScale, boolean scaleX, boolean scaleY, boolean perfectScale) {
+        float objectScaleX = screenWidth / 1920f;
+        float objectScaleY = screenHeight / 1080f;
+
+        if (scaleX) {
+            width = Math.round((width * guiScale) * objectScaleX);
+        }
+
+        if (scaleY) {
+            height = Math.round((height * guiScale) * objectScaleY);
+        }
+
+        if (perfectScale) {
+            if (scaleX) {
+                height = Math.round((height * guiScale) * objectScaleX);
+            }
+
+            if (scaleY) {
+                width = Math.round((width * guiScale) * objectScaleY);
+            }
+        }
+
+        widget.setSize(width, height);
     }
 }
